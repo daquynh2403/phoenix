417d1f59edf12753a239891632eb70016d160bcb
daquynh2403 <daquynh2403@gmail.com>
2024-01-05 07:31:04 +0700

    Refactor: portfolio section

---
+++ src/components/Project.jsx
@@ -0,0 +1,31 @@
+import project3 from '../assets/project-3.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faReact, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+
+function Project() {
+  return (
+    <div className='w-full flex border-t-2 justify-center gap-40'>
+      <div className="p-4">
+        <span className="text-5xl text-slate-700 font-thin underline">
+          Lunari
+        </span>
+        <span className="text-5xl text-slate-900 font-bold">
+          Market
+        </span>
+        <div className="flex ">S
+          <FontAwesomeIcon icon={faGithub} size='xl'/>
+          <span className='text-slate-900 text-xl mr-2 hover:underline transition duration-700 ease-in-out'>Live Demo</span>
+          <FontAwesomeIcon icon={faArrowUpRightFromSquare} size='xl'/>
+        </div>
+        <div className="font-semibold text-slate-900">An E-commerce website allows users shopping online</div>
+        <FontAwesomeIcon  icon={faReact} style={{color: '#417ce1'}} size="2xl"/>
+      </div>
+      <div className="overflow-hidden w-1/3" >
+        <img src={project3} alt='Project 1' className='w-full h-full transition duration-700 ease-in-out hover:opacity-85 hover:scale-125'/>
+      </div>
+    </div>
+  )
+}
+
+export default Project
