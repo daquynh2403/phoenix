543f01aa67791b81946bfb715a4361a58ed34677
luna.le <luna.le@i-on.net>
2024-01-05 17:24:39 +0700

    Style: about section

--- src/components/Introduce.jsx
+++ src/components/Introduce.jsx
@@ -1,8 +1,8 @@
 import logo from "../assets/introduce.jpg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLocationDot, faSquarePhoneFlip } from '@fortawesome/free-solid-svg-icons'
+import { faLocationDot, faSquarePhoneFlip,  } from '@fortawesome/free-solid-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
-import { faHtml5, faCss3Alt, faJs, faReact, faVuejs, faSass, faNodeJs, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faHtml5, faCss3Alt, faJs, faReact, faVuejs, faSass, faNodeJs, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 function Introduce() {
   const techStackData= [
@@ -15,29 +15,39 @@ function Introduce() {
     { icon: faNodeJs, color: '#5ebe5b'}
   ]
   const socialLinksData = [
-    { icon: faLinkedin, color: '#397aea'},
-    { icon: faGithub, color: ''},
-    { icon: faEnvelope, color: '#ea1f1f'},
-    { icon: faSquarePhoneFlip, color: ''}
+    { icon: faLinkedinIn, color: '', link: 'daquynh2403@gmail.com'},
+    { icon: faGithub, color: '', link: 'daquynh2403@gmail.com'},
+    { icon: faEnvelope, color: '', link: 'daquynh2403@gmail.com'},
+    { icon: faSquarePhoneFlip, color: '', link: 'daquynh2403@gmail.com'}
   ]
   return (
     <div className="w-fit mx-auto flex mt-16">
         <div className="mr-8">
-          <div className="text-5xl font-bold">
-            Front-End Developer
-          </div>
-          <div className="text-lg text-slate-500 my-16">
+          <span className="text-5xl font-normal text-slate-700 mr-4 underline decoration-1 underline-offset-8">
+            Front-End
+          </span>
+          <span className="text-5xl font-bold">
+            Developer
+          </span>
+          <div className="text-lg text-slate-500 mt-10">
             <span>Hi, I am Quynh Le. A passionate Front-end Developer based in </span>
             <span className="font-semibold text-slate-600">
                 Ho Chi Minh, Vietnam 
                 <FontAwesomeIcon icon={faLocationDot} size="lg" className="ml-1" />
             </span>
           </div>
-          <div className="flex gap-4 items-center font-bold mt-12">
-            <span className="border-r-2 border-slate-600 pr-3 py-1">Social links</span>
-            {socialLinksData.map((item, index) => (
-              <FontAwesomeIcon key={index} icon={item.icon} style={{color: item.color}} size="2xl"/>
-            ))}
+          <div className="p-4 border-4 border-double border-slate-200 rounded-lg mt-4">
+            <div className="w-[40rem] break-words text-xl font-thin text-slate-900">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus magni itaque aut explicabo aspernatur eius natus beatae voluptatem veritatis odio, quisquam hic voluptates cumque sit atque architecto expedita vel soluta.
+            </div>
+            <div className="mt-10 font-normal text-slate-700">
+              {socialLinksData.map((item, index) => (
+                <div key={index} className="mt-3">
+                  <FontAwesomeIcon key={index} icon={item.icon} style={{color: item.color}} size="2xl" className="w-10 mr-2"/>
+                  <a href="#" className="">{item.link}</a>
+                </div>
+              ))}
+            </div>
           </div>
           <div className="flex gap-4 items-center font-bold mt-10">
             <span className="border-r-2 border-slate-600 pr-4 py-1">Tech stack</span>
--- src/components/ProjectShowcase.jsx
+++ src/components/ProjectShowcase.jsx
@@ -2,14 +2,19 @@ import Project from './Project'
 
 function ProjectShowcase() {
   return (
-    <>
-      <div>Portfolio </div>
-      <div>Search </div>
+    <div className=''>
+      <div className='text-slate-700 font-normal text-5xl border-l-2 border-slate-300 w-2/3 mx-auto pl-2 py-3'>Portfolio </div>
+      <div>
+        <div>All</div>
+        <div>Fullstack</div>
+        <div>Frontend Only</div>
+        <div>UI/UX Designer</div>
+      </div>
       <Project />
       <Project />
       <Project />
       <Project />
-    </>
+    </div>
     
   )
 }
